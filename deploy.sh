8951ca340aae004ea7b70208d6af3a082c4398da
xscrat <xjfscrat@gmail.com>
2019-08-22 00:00:36 +0800

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+sudo apt-get -y install node.js
+sudo apt-get -y install npm
+sudo npm install -g cnpm@4.1.0 --registry=https://registry.npm.taobao.org
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo cnpm install -g electron --registry=https://registry.npm.taobao.org
+sudo apt-get -y install python-pip
+sudo pip install serial
