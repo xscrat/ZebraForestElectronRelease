a00adb7af62c7711a91ba8aa33aef45134e2cd47
xscrat <xjfscrat@gmail.com>
2019-08-31 00:04:52 +0800

    Add sources.list

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,35 @@
+echo "******************* begin deploy"
 cat /etc/apt/sources.list | grep 'ali' &> /dev/null
 if [ $? != 0 ]; then
 	sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
 	sudo cp sources.list /etc/apt/sources.list
+	echo "******************* begin update"
 	sudo apt-get update
 fi
+echo "******************* begin install node.js"
 which node | grep / &> /dev/null
 if [ $? != 0 ]; then
-	sudo apt-get -y install node.js
+	sudo apt-get -y install nodejs
 fi
 sudo ln -s /usr/bin/nodejs /usr/bin/node
+echo "******************* begin install npm"
 which npm | grep / &> /dev/null
 if [ $? != 0 ]; then
 	sudo apt-get -y install npm
 fi
+echo "******************* begin install cnpm"
 which cnpm | grep / &> /dev/null
 if [ $? != 0 ]; then
 	sudo npm install -g cnpm@4.1.0 --registry=https://registry.npm.taobao.org
 fi
+echo "******************* begin install electron"
 which electron | grep / &> /dev/null
 if [ $? != 0 ]; then
 	sudo cnpm install -g electron@6.0.0 --registry=https://registry.npm.taobao.org
 fi
+echo "******************* begin install serial"
 sudo apt-get -y install python3-pip
 sudo pip3 install serial
+echo "******************* begin start"
 electron .
 
