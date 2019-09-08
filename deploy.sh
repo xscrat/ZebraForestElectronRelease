69e12097abfdd7f25ba075c93b3a8ca3bbf49e6f
xscrat <xjfscrat@gmail.com>
2019-09-08 16:20:12 +0800

    Add electron prompt

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 echo "******************* begin deploy"
-cat /etc/apt/sources.list | grep 'ali' &> /dev/null
+ls node_modules &> /dev/null
 if [ $? != 0 ]; then
-	sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
-	sudo cp sources.list /etc/apt/sources.list
-	echo "******************* begin update"
-	sudo apt-get update
+	tar xvf node_modules.tar.gz
+fi
+echo "******************* begin install serial"
+pip3 freeze | grep serial &> /dev/null
+if [ $? != 0 ]; then
+	sudo apt-get -y install python3-pip
+	sudo pip3 install serial
 fi
 echo "******************* begin install node.js"
 which node | grep / &> /dev/null
@@ -17,19 +20,6 @@ which npm | grep / &> /dev/null
 if [ $? != 0 ]; then
 	sudo apt-get -y install npm
 fi
-echo "******************* begin install cnpm"
-which cnpm | grep / &> /dev/null
-if [ $? != 0 ]; then
-	sudo npm install -g cnpm@4.1.0 --registry=https://registry.npm.taobao.org
-fi
-echo "******************* begin install electron"
-which electron | grep / &> /dev/null
-if [ $? != 0 ]; then
-	sudo cnpm install -g electron@6.0.0 --registry=https://registry.npm.taobao.org
-fi
-echo "******************* begin install serial"
-sudo apt-get -y install python3-pip
-sudo pip3 install serial
 echo "******************* begin start"
-electron .
+node_modules/electron/cli.js .
 
